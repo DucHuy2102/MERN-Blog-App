8d79f38d3f4b3a3e3cd1f2ed10576e34e1110916
NguyenDucHuy <duchuytv2102@gmail.com>
2024-08-03 00:32:47 +0700

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGODB)
+    .then(() => {
+        console.log('\n--> Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!!');
+    console.log('\n--> Server is running on port 3000!');
 });
