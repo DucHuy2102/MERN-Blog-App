d46de0232f7919ed7c1943a80ae4c9d126d03da1
Ng.DucHuy <duchuytv2102@gmail.com>
2024-08-16 14:19:06 +0700

    create get all posts api route

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import Post from '../models/post.modal.js';
 import { handleError } from '../utils/handleError.js';
 
+// create a new post
 export const createPost = async (req, res, next) => {
     if (!req.user.isAdmin) {
         return next(handleError(403, 'You are not authorized to create a post'));
@@ -23,6 +24,41 @@ export const createPost = async (req, res, next) => {
     try {
         const savedPost = await newPost.save();
         res.status(201).json(savedPost);
+    } catch (error) {
+        next(handleError(500, error.message));
+    }
+};
+
+// get all posts
+export const getPosts = async (req, res, next) => {
+    console.log(req.query);
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.perPage) || 9;
+        const sort = req.query.order === 'asc' ? 1 : -1;
+        const posts = await Post.find({
+            ...(req.query.userID && [{ userID: req.query.userID }]),
+            ...(req.query.category && [{ category: req.query.category }]),
+            ...(req.query.slug && [{ slug: req.query.slug }]),
+            ...(req.query.postID && [{ _id: req.query.postID }]),
+            ...(req.query.searchTerm && {
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } },
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } },
+                ],
+            }),
+        })
+            .sort({ updatedAt: sort })
+            .skip(startIndex)
+            .limit(limit);
+        const totalPosts = await Post.countDocuments();
+        const now = new Date();
+        const lastMonth = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+        const postsLastMonth = await Post.countDocuments({
+            createdAt: { $gte: lastMonth },
+        });
+
+        res.status(200).json({ posts, totalPosts, postsLastMonth });
     } catch (error) {
         next(handleError(500, error.message));
     }
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
+// create a new post
 router.post('/create-post', verifyUser, createPost);
+
+// get all posts
+router.get('/get-posts', getPosts);
 
 export default router;
